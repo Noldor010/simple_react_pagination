416fb3ed228ce9104834d4433d5d8cc646b94851
Noldor010 <qwerrnoldor2@gmail.com>
2022-12-24 17:54:22 +0200

    Updates

---
+++ precache-manifest.b4a7a5ed6814e3a4d47f9e1a46298b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306194f50d7a5172ff5a3fb0e710852e",
+    "url": "/simple_react_pagination/index.html"
+  },
+  {
+    "revision": "075465cf10c990221758",
+    "url": "/simple_react_pagination/static/css/main.e88b7dc2.chunk.css"
+  },
+  {
+    "revision": "4dea63ec98f4a4958f92",
+    "url": "/simple_react_pagination/static/js/2.02cc656a.chunk.js"
+  },
+  {
+    "revision": "075465cf10c990221758",
+    "url": "/simple_react_pagination/static/js/main.b5f966ea.chunk.js"
+  },
+  {
+    "revision": "305b662c052fa4a6f09b",
+    "url": "/simple_react_pagination/static/js/runtime~main.28b8ea8f.js"
+  }
+]);
